4e32bac5ff6cbc9eda161ec1a37f5fc0c880f1db
Connor Hood <chood531@gmail.com>
2015-10-17 20:15:44 -0500

    all models finished

--- app/Team.php
+++ app/Team.php
@@ -5,8 +5,33 @@ use Laravel\Cashier\Billable;
 use Laravel\Cashier\Contracts\Billable as BillableContract;
 
 use Laravel\Spark\Teams\Team as SparkTeam;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Team extends SparkTeam implements BillableContract
 {
-    use Billable;
+    use Billable, SoftDeletes;
+
+    protected $columns = [
+        'id',
+        'owner_id',
+        'name',
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    public function projects()
+    {
+        return $this->hasMany('CMV\Models\PM\Project');
+    }
+
+    public function invoices()
+    {
+        return $this->hasManyThrough('CMV\Models\PM\Invoice','CMV\Models\PM\Project');
+    }
+
+    public function conciergeSites()
+    {
+        return $this->hasMany('CMV\Models\PM\ConciergeSite');
+    }
 }
