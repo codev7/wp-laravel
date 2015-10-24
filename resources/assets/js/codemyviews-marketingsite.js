6ec752849877829d34a32e07ea9d48f024141f3e
Connor Hood <chood531@gmail.com>
2015-10-24 16:17:25 -0500

    ...

--- resources/assets/js/codemyviews-marketingsite.js
+++ resources/assets/js/codemyviews-marketingsite.js
@@ -1,46 +1,5 @@
-require('laravel-spark/core/dependencies');
-
-window.$ = window.jQuery = require('jquery');
+//require('laravel-spark/core/dependencies');
 
 
-require('./plugins/jquery.ui.core');
-require('./plugins/moment');
-require('./plugins/jquery.waypoints');
-require('./plugins/jquery.validate');
-require('./plugins/bootstrap.min');
-require('./plugins/bugsnag');
-require('./plugins/custom-form');
-require('./plugins/datepicker');
-require('./plugins/highlight.pack');
-require('./plugins/image-grid');
-require('./plugins/jquery.matchHeight-min');
-require('./plugins/jquery.payment');
-require('./plugins/jquery.Scrollto');
-require('./plugins/jquery.waypoint.sticky');
-require('./plugins/lz');
-require('./plugins/number_format');
-require('./plugins/placeholder.min');
-require('./plugins/scroll-tracker');
-require('./plugins/slick');
-require('./plugins/tablesorter');
-require('./plugins/zero-clipboard');
-require('./plugins/zoom');
-
-require('./custom/cmv-helpers.js');
-require('./custom/validation.js');
-require('./custom/bugsnag');
-require('./custom/chartjs-data-api');
-require('./custom/chatlio');
-require('./custom/click-to-copy');
-require('./custom/custom-form-file');
-require('./custom/custom');
-require('./custom/datepicker');
-require('./custom/job-application');
-require('./custom/load-more');
-require('./custom/scripts');
-
-require('./Vue/concierge');
-require('./Vue/portfolio');
-require('./Vue/quote');
 
 
