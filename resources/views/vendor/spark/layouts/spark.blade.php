6489c6425da09e652aabd6c177e667c5ce0060a2
Gregory Buzdyk <buzdyk.gregory@gmail.com>
2015-11-10 18:14:56 +0300

    move nav-authenticated out of the pjax container

--- resources/views/vendor/spark/layouts/spark.blade.php
+++ resources/views/vendor/spark/layouts/spark.blade.php
@@ -7,18 +7,16 @@
     <!-- Vue App For Spark Screens -->
     <div id="spark-app" v-cloak>
         <div class="growl" id="app-growl"></div>
-            <div id="pjax-container">
-                <!-- Navigation -->
-                @if (Auth::check())
-                    @include('spark::nav.authenticated')
-                @else
-                    @include('spark::nav.guest')
-                @endif
+            <!-- Navigation -->
+            @if (Auth::check())
+                @include('spark::nav.authenticated')
+            @else
+                @include('spark::nav.guest')
+            @endif
 
-                <div class="container p-t-md">
-                    @yield('content')
-                </div><!--container-->
-            </div>
+            <div class="container p-t-md" id="pjax-container">
+                @yield('content')
+            </div><!--container-->
         <!-- Footer -->
         @include('spark::common.footer')
 
