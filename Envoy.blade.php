97b69e97a0346173d40408e7418c866b23d5d2ae
Ivan Kulikov <ikoolik@gmail.com>
2015-11-11 22:33:00 +0300

    change relative path to absolute

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,21 +1,22 @@
 @setup
     $domain = "{$subdomain}.approvemyviews.com";
+    $path = "/home/forge/{$domain}";
 @endsetup
 
 @servers(['forge' => 'forge@cmvplatform.approvemyviews.com'])
 
 @task('init', ['on' => 'forge'])
     echo 'going to initialize {{$domain}} from {{$repo}}'
-    echo 'rm -rf ./{{$domain}}'
-    rm -rf ./{{$domain}}
-    echo 'git clone {{$repo}} {{$domain}}';
-    git clone {{$repo}} {{$domain}}
+    echo 'rm -rf {{$path}}'
+    rm -rf {{$path}}
+    echo 'git clone {{$repo}} {{$path}}';
+    git clone {{$repo}} {{$path}}
 @endtask
 
 @task('deploy', ['on' => 'forge'])
     echo 'going to deploy {{$domain}} from {{$repo}}'
-    echo 'cd ./{{$domain}}';
-    cd ./{{$domain}}
+    echo 'cd {{$path}}';
+    cd {{$path}}
     echo 'git pull';
     git pull
 @endtask
