94017580fa3423f2717c1d722ccd497de2c90628
Connor Hood <chood531@gmail.com>
2016-01-02 21:08:05 -0600

    fix mobile scroll

--- resources/views/common/head.blade.php
+++ resources/views/common/head.blade.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
 <meta id="token" name="csrf-token" content="{{ csrf_token() }}"/>
 <meta id="stripe-token" name="stripe-token" content="{{ env('STRIPE_PUBLIC') }}"/>
 
